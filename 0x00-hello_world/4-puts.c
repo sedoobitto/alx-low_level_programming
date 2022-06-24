2bba8022290a80bb31f277ab55eec0a3bed6a58b
Sedoo Bitto <sedoobitto@gmail.com>
2022-06-24 16:44:25 -0700

    task 4 corr

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+
+/**
+* main - prints a sentence
+* Return: 0 if exited properly, non-zero otherwise
+*/
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
