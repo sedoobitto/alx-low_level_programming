fc2a2e427bdb9e740ac54f729aeaadaec8598fbf
Sedoo Bitto <sedoobitto@gmail.com>
2022-06-24 16:57:08 -0700

    Task6 unsure corr

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,6 +6,13 @@
 */
 int main(void)
 {
+printf("size32\n");
+printf("Size of char: %d byte(s)\n", sizeof(char));
+printf("Size of an int: %d byte(s)\n", sizeof(int));
+printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+printf("Size of a float: %d byte(s)\n", sizeof(float));
+printf("size64\n");
 printf("Size of char: %d byte(s)\n", sizeof(char));
 printf("Size of an int: %d byte(s)\n", sizeof(int));
 printf("Size of a long int: %d byte(s)\n", sizeof(long int));
@@ -13,4 +20,3 @@ printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 printf("Size of a float: %d byte(s)\n", sizeof(float));
 return (0);
 }
-
