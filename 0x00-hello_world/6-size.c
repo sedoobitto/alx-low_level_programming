809fc6fec4d6c01730cacbac7788d57b274f8f18
Sedoo Bitto <sedoobitto@gmail.com>
2022-06-25 00:16:29 -0700

    t6 cor

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,6 +15,6 @@ printf("Size of an int: %d byte(s)\n", sizeof(int));
 printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 printf("Size of a float: %d byte(s)\n", sizeof(float));
-fprintf(stderr, "anything");
+fprintf(stderr, [Anything]);
 return (0);
 }
