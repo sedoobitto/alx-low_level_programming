d341bdec32d5c2088ea4d8f1682a0ac3ac8b48c1
Sedoo Bitto <sedoobitto@gmail.com>
2022-06-25 06:39:22 -0700

    removes unnecessary lines

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,15 +6,11 @@
 */
 int main(void)
 {
-int intType;
-float floatType;
-double doubleType;
-char charType;
 printf("Size of char: %i byte(s)\n", sizeof(char));
 printf("Size of an int: %i byte(s)\n", sizeof(int));
 printf("Size of a long int: %i byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
 printf("Size of a float: %i byte(s)\n", sizeof(float));
-fprintf(stderr, "%s", "[Anything]\n");
+fprintf(stderr, "%s", "[Anything]");
 return (0);
 }
