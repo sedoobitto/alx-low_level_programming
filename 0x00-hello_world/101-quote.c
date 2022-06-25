877e307370f80ebe7750800ffc6ba92cd03834a7
Sedoo Bitto <sedoobitto@gmail.com>
2022-06-24 17:07:28 -0700

    Dunno task8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,13 @@
 #include <unistd.h>
 
 /**
-* main - prints exactly "and that piece of art is useful"
-* - Dora Korpar, 2015-10-19",
+* main - prints exactly and that piece of art is useful
+* - Dora Korpar 
 * followed by a new line, to the standard error.
-* Return: Always 0 (Success)
+* Return: standard error
 */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" 59);
+write ("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" );
 return (1);
 }
