b2da94a43f20d42711bb2f71655fab530007edb8
Sedoo Bitto <sedoobitto@gmail.com>
2022-06-27 09:34:58 -0700

    cor

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,6 +9,7 @@
 */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+char quote[] = "and that piece of art is useful" - Dora Korpar, 2015-10-19";
+write(2, quote, sizeof(quote));
 return (1);
 }
