c1dd3ee73d58028aeea4a23b2f85262ba6aca53e
Sedoo Bitto <sedoobitto@gmail.com>
2022-06-27 23:27:13 -0700

    corrections

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include <unistd.h>
 /**
 * main - prints exactly and that piece of art is useful
-* - Dora Korpar 
+* - Dora Korpar
 * followed by a new line, to the standard error.
 * Return: Always 1 (Success)
 */
@@ -9,5 +9,5 @@ int main(void)
 {
 char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 write(2, quote, 59);
-return (1);;
+return (1);
 }
