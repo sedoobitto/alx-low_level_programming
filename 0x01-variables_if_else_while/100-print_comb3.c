f270cef7274e803d03ab3e2a7b2adc95f6284a4e
Sedoo Bitto <sedoobitto@gmail.com>
2022-07-01 04:17:49 -0700

    correction

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,31 +1,27 @@
 #include <stdio.h>
-
 /**
- * main - Prints numbers between 00 to 89.
- *
- * Return: Always 0 (Success)
- */
+* main - Prints numbers between 00 to 99.
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-	int i, e;
-	i = 48;
-	e = 48;
+int i, e;
+i = 48;
+e = 48;
 while (e < 58)
 {
 i = 48;
 while (i < 58)
 {
-if (e != i && e < i)
-{
 putchar(e);
 putchar(i);
-if (i == 57 && e == 56)
-}
+if (i == 57 && e == 57)
+{
 break;
 }
 putchar(',');
 putchar(' ');
-}
 i++;
 }
 e++;
