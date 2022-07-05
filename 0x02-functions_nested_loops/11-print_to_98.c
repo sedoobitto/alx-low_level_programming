a3fddb0607a9c1c4e3593978648d1f5d9ba77ab6
Sedoo Bitto <sedoobitto@gmail.com>
2022-07-05 10:55:17 -0700

    fixing

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,15 +14,15 @@ void print_to_98(int n)
 	{
 		for (; n > 98; n--)
 		{
-			_putchar("%d, ", n);
+			printf("%d, ", n);
 		}
 	}
 	else if (n < 98)
 	{
 		for (; n < 98; n++)
 		{
-			_putchar("%d, ", n);
+			printf("%d, ", n);
 		}
 	}
-	_putchar("%d\n", n);
+	printf("%d\n", n);
 }
