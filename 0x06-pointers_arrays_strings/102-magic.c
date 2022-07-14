4c24058ca0525426b528c1b4fae1db5db0657090
Sedoo Bitto <sedoobitto@gmail.com>
2022-07-14 04:48:35 -0700

    fixing

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -24,7 +24,6 @@ a[2] = 98;
 * - only one statement
 * - you are not allowed to code anything else than this line of code
 */
-;
 /* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
 return (0);
